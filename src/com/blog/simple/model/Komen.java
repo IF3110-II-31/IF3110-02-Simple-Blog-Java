b3f30f3b858fbea607c00ce49f8912d0a18bc53e
Ramandika Pranamulia <theone_rama@yahoo.com>
2014-11-28 15:10:07 +0700

    Kelas Komen(bukan bean)

---
+++ src/com/blog/simple/model/Komen.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ramandika
+ */
+package com.mycompany.wbd;
+public class Komen {
+    private int idpost;
+    private String commentator;
+    private String email;
+    private String comment_content;
+
+    public int getIdpost() {
+        return idpost;
+    }
+
+    public void setIdpost(int idpost) {
+        this.idpost = idpost;
+    }
+
+    public String getCommentator() {
+        return commentator;
+    }
+
+    public void setCommentator(String commentator) {
+        this.commentator = commentator;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getComment_content() {
+        return comment_content;
+    }
+
+    public void setComment_content(String comment_content) {
+        this.comment_content = comment_content;
+    }
+}
